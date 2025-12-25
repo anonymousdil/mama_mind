0b34aa842c845d5f5f5eb1081aaefd985fc2bd5c
Mohammed Dil Barash <dmohamme@gitam.in>
2025-12-26 00:22:20 +0530

    Add Firebase configuration and initialization script

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,20 @@
+//firebase config
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAtwkoUPEK7saTfY1CExt7OGTmPxrOr0-k",
+    authDomain: "mamamind-29f5c.firebaseapp.com",
+    projectId: "mamamind-29f5c",
+    storageBucket: "mamamind-29f5c.firebasestorage.app",
+    messagingSenderId: "508802998644",
+    appId: "1:508802998644:web:ae458bb6fc31b4623e0c21"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
