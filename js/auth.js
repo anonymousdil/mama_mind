f88511e16050e411281e611ae7196644a3327e3a
Mohammed Dil Barash <dmohamme@gitam.in>
2025-12-26 00:23:17 +0530

    Implement user login with role assignment
    Added user role handling during login and document creation.

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,20 @@
+import { auth, db } from "./firebase.js";
+import { signInWithEmailAndPassword } from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { doc, setDoc } from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+document.getElementById("loginBtn").onclick = async () => {
+  const email = email.value;
+  const password = password.value;
+  const role = role.value;
+
+  const user = await signInWithEmailAndPassword(auth, email, password);
+
+  await setDoc(doc(db, "users", user.user.uid), {
+    email,
+    role
+  });
+
+  window.location.href = role + ".html";
+};
