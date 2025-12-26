0d69e21c2a1b16dbc69aea3e548180cd0495ebc8
Mohammed Dil Barash <dmohamme@gitam.in>
2025-12-26 10:56:58 +0530

    Add Firebase Auth and Firestore imports and exports

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,21 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase core
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAtwkoUPEK7saTfY1CExt7OGTmPxrOr0-k",
-    authDomain: "mamamind-29f5c.firebaseapp.com",
-    projectId: "mamamind-29f5c",
-    storageBucket: "mamamind-29f5c.firebasestorage.app",
-    messagingSenderId: "508802998644",
-    appId: "1:508802998644:web:ae458bb6fc31b4623e0c21"
-  };
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAtwkoUPEK7saTfY1CExt7OGTmPxrOr0-k",
+  authDomain: "mamamind-29f5c.firebaseapp.com",
+  projectId: "mamamind-29f5c",
+  storageBucket: "mamamind-29f5c.appspot.com",
+  messagingSenderId: "508802998644",
+  appId: "1:508802998644:web:ae458bb6fc31b4623e0c21"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
